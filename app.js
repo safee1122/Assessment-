1472059ba0a42239ccbd2e23973a16b421450dcd
safee1122 <safee.saqib1122@gmail.com>
2023-12-04 12:02:06 +0500

    added frontend build

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/uploads', express.static(path.join(__dirname, '/src/uploads')));
 var corsOptions = {
     origin: "*",
